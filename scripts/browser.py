cfd935bccdab591bbebc35816f22f933537995fb
EMIT2019 <emilianoandreurbinazavala@gmail.com>
2025-07-01 23:47:45 -0600

    Changed the logic and usage of check_dom_element method for avoiding amazon antibots layouts and added a method for making multiple or a single search

--- scripts/browser.py
+++ scripts/browser.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -7,23 +8,27 @@ from selenium.webdriver import Chrome
 from selenium_stealth import stealth
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement
 
 class Browser:
-    def __init__(self, web_driver_path:str):
+    def __init__(self, web_driver_path:str, web_url:str, page_elements_metadata:dict):
         self.web_driver_path:str = web_driver_path
+        self.web_page_url:str = web_url
         self.driver:Chrome = None
+        self.page_elements:dict = page_elements_metadata
 
-    def open_browser(self) -> None:
+    def open_web_page(self) -> None:
         options = Options()
         options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_argument("--headless")
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+        options.add_argument("--start-maximized")
         service = Service(self.web_driver_path)
-        self.driver = webdriver.Chrome(service=service)
+        self.driver = webdriver.Chrome(service=service, options=options)
         
         #Bypassing Captchas
         stealth(
             self.driver,
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
             languages=["en-US", "en"],
             vendor="Google Inc.",
             platform="Win32",
@@ -31,20 +36,54 @@ class Browser:
             renderer="Intel Iris OpenGL Engine",
             fix_hairline=True
         )
-        #self.driver.get('https://bot.sannysoft.com')
-        self.driver.get('https://www.amazon.com')
 
-    def check_dom_element(self, element_xpath:str) -> bool:
+        self.driver.get(self.web_page_url)
+
+    def check_dom_element(self, id:str = None, xpath:str = None) -> bool:
         try:
+            parameter = None
+            element_value:str = ""
+
+            if id is not None:
+                parameter = By.ID
+                element_value = id
+            elif xpath is not None:
+                element_value = xpath
+                parameter = By.XPATH
+
             element_exists:bool = False
             wait = WebDriverWait(self.driver, 10)
-            wait.until(EC.visibility_of_element_located((By.XPATH, element_xpath)))
-            wait.until(EC.element_to_be_clickable((By.XPATH, element_xpath)))
-            
-            search_bar = self.driver.find_element(By.XPATH, element_xpath)
-            if search_bar.is_displayed() and search_bar.is_enabled():
+            wait.until(EC.visibility_of_element_located((parameter, element_value)))
+            wait.until(EC.element_to_be_clickable((parameter, element_value)))
+            dom_element = self.driver.find_element(parameter, element_value)
+            if dom_element.is_displayed() and dom_element.is_enabled():
                 element_exists = True
-            return element_exists
+                return {"flag":element_exists, "element":dom_element}
+            else:
+                element_exists = False
+                return {"flag":element_exists, "element":dom_element}
+        except Exception as ex:
+            print(f"Something went wrong during the execution. This is the full error message: {ex}")
+            return {"flag":False, "element":None}
+
+    def make_search(self, search:list[str] | str = None) -> None:
+        try:
+            element_id:str = self.page_elements["search_bar"]["id"]
+            flag = self.check_dom_element(id=element_id)["flag"]
+            if flag:
+                if isinstance(search, list):
+                    for item in search:
+                        search_bar:WebElement = self.check_dom_element(id=element_id)["element"]
+                        search_bar.clear()
+                        search_bar.send_keys(item)
+                        search_bar.send_keys(Keys.ENTER)
+                        time.sleep(3)
+                elif isinstance(search, str):
+                    search_bar.send_keys(search)
+                    search_bar.send_keys(Keys.ENTER)
+                    time.sleep(3)
+            else:
+                self.close_browser()
         except Exception as ex:
             print(f"Something went wrong during the execution. This is the full error message: {ex}")
 
--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,42 @@
 from browser import Browser
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.keys import Keys
 import time
 
 def main() -> None:
     driver_path:str = r"resources\drivers\chromedriver.exe"
-    search_bar_xpath:str = "/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[2]/div[1]/input"
+    page_elements_metadata:dict = {
+        "search_bar": {
+            "id": "twotabsearchtextbox",
+            "alt_id": "nav-bb-search"
+        },
+        "security_button": {
+            "xpath": "/html/body/div/div[1]/div[3]/div/div/form/div/div/span/span/button"
+        }
+    }
 
-    browser:Browser = Browser(driver_path)
-    browser.open_browser()
-    value = browser.check_dom_element(search_bar_xpath)
-    print(f"THIS IS THE VALUE OF THE VARIABLE VALUE: {value}")
-    time.sleep(30)
-    browser.close_browser()
+    url:str = 'https://www.amazon.com/?language=en_US&currency=USD'
+    search_list:list[str] = ["MackBook Air Pro", "Samsung Galaxy S20", "Office chair", "Office desktop", "A/C", "LED Ligths"]
+    browser:Browser = Browser(driver_path, url, page_elements_metadata)
+    try:
+        browser.open_web_page()
+        values = browser.check_dom_element(xpath=page_elements_metadata["security_button"]["xpath"])
+
+        if values["flag"]:
+            values["element"].click()
+
+        values:dict = browser.check_dom_element(id=page_elements_metadata["search_bar"]["alt_id"])
+
+        if values["flag"]:
+            values["element"].click()
+            values["element"].send_keys(Keys.ENTER)
+
+        values = browser.check_dom_element(id=page_elements_metadata["search_bar"]["id"])
+        browser.make_search(search_list)
+
+    except Exception as ex:
+        print(f"THIS IS THE ERROR MESSAGE: {ex}")
+        browser.close_browser()
+
 if __name__ == "__main__":
     main()
-    
